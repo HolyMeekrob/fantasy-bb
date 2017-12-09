df224b2b3cb498280545deb8edaa983e82d2577b
Holy Meekrob <holy.meekrob@gmail.com>
2017-12-09 14:32:35 -0600

    Add elm to build

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,3 +19,13 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+import Elm from './elm';
+
+const elmDiv = document.querySelector('#elm_target');
+
+if (elmDiv) {
+	Elm.Main.embed(elmDiv)
+} else {
+	console.error('Elm cannot be embedded. Missing div with id="elm_target"');
+}
