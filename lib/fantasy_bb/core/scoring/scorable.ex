ca70e08fcf4d6a4b3378926edc70fefbedaa1215
Holy Meekrob <holy.meekrob@gmail.com>
2018-04-12 21:55:53 -0500

    Implement scoring for winning a miscellaneous competition

--- lib/fantasy_bb/core/scoring/scorable.ex
+++ lib/fantasy_bb/core/scoring/scorable.ex
@@ -183,6 +183,11 @@ defmodule FantasyBb.Core.Scoring.Scorable do
     true
   end
 
+  # Win miscellaneous competition
+  def should_process(34, %League{events: [%Event{} = event | _remaining]}) do
+    event.event_type_id === 11
+  end
+
   def should_process(_event_type_id, _events) do
     false
   end
@@ -440,6 +445,11 @@ defmodule FantasyBb.Core.Scoring.Scorable do
     league = Enum.reduce(curr.season.voters, curr, add_points)
 
     {prev, league}
+  end
+
+  # Win miscellaneous competition
+  def process(34, points, prev, curr) do
+    award_points_to_event_assignee(points, prev, curr)
   end
 
   def process(_event_type_id, _points, prev, curr) do
