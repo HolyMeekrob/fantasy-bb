37e4fb66617f713de068ef9b8eafdd3fc58d8926
Holy Meekrob <holy.meekrob@gmail.com>
2018-05-04 23:13:37 -0500

    Only include user's leagues in user leagues page

--- lib/fantasy_bb/data/league/queries.ex
+++ lib/fantasy_bb/data/league/queries.ex
@@ -9,8 +9,17 @@ defmodule FantasyBb.Data.League.Queries do
   end
 
   def for_user(query, user_id) do
+    user_leagues_query =
+      from(
+        league in League,
+        inner_join: teams in assoc(league, :teams),
+        where: teams.user_id == ^user_id
+      )
+
     from(
       league in query,
+      inner_join: user_league in subquery(user_leagues_query),
+      on: user_league.id == league.id,
       left_join: season in assoc(league, :season),
       left_join: teams in assoc(league, :teams),
       preload: [season: season, teams: teams]
